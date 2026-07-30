e7fc562cc012925a7a8840607bab24b229fe6422
Arjun Girirajkumar <arjungirirajkumar@gmail.com>
2026-07-30 14:20:22 +0530

    chore: update code to follow zig 0.16 syntax

--- src/makemore/main.zig
+++ src/makemore/main.zig
@@ -1,18 +1,20 @@
 const std = @import("std");
 
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+pub fn main(init: std.process.Init) !void {
+    const io = init.io;
+
+    var gpa = std.heap.DebugAllocator(.{}){};
     defer _ = gpa.deinit();
     const a = gpa.allocator();
 
     // load the file into heap memory
-    const content = try std.fs.cwd().readFileAlloc(a, "names.txt", 5 * 1024 * 1024);
+    const content = try std.Io.Dir.cwd().readFileAlloc(io, "names.txt", a, .limited(5 * 1024 * 1024));
     defer a.free(content);
 
     // inputs and corresponding outputs of the dataset. 0 = . & 1-26 = a-z (convenience)
-    var xa = std.ArrayList(u8){};
+    var xa: std.ArrayList(u8) = .empty;
     defer xa.deinit(a);
-    var ya = std.ArrayList(u8){};
+    var ya: std.ArrayList(u8) = .empty;
     defer ya.deinit(a);
 
     // populate the xa and ya
@@ -20,12 +22,12 @@ pub fn main() !void {
     var c: usize = 0;
     while (words.next()) |word| {
         try xa.append(a, 0);
-        try ya.append(a, word[0]-'a'+1);
-        for (0..word.len-1) |i| {
-            try xa.append(a, word[i]-'a'+1);
-            try ya.append(a, word[i+1]-'a'+1);
+        try ya.append(a, word[0] - 'a' + 1);
+        for (0..word.len - 1) |i| {
+            try xa.append(a, word[i] - 'a' + 1);
+            try ya.append(a, word[i + 1] - 'a' + 1);
         }
-        try xa.append(a, word[word.len-1]-'a'+1);
+        try xa.append(a, word[word.len - 1] - 'a' + 1);
         try ya.append(a, 0);
 
         c += 1;
@@ -33,14 +35,14 @@ pub fn main() !void {
     }
 
     // one-hot encode the input array
-    var xenc = std.ArrayList([27]u8){};
+    var xenc: std.ArrayList([27]u8) = .empty;
     defer xenc.deinit(a);
     for (xa.items) |x| {
         var s: [27]u8 = .{0} ** 27;
         s[x] = 1;
         try xenc.append(a, s);
     }
-    
+
     for (xenc.items) |x| {
         for (x) |b| {
             std.debug.print("{d}", .{b});
